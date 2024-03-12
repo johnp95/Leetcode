6726e4b3fcc1e5df5311f59a91b4a7bc41adf59e
John Pennyman <johnpennyman95@gmail.com>
2024-03-12 11:52:15 -0500

    26

---
+++ NeetCode 150/2-AddTwoNumbers.py
@@ -0,0 +1,38 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def addTwoNumbers(self, l1,l2):
+
+        dummy = ListNode()
+        curr = dummy
+        carry = 0
+
+        while l1 or l2 or carry:
+            v1 = l1.val if l1 else 0
+            v2 = l2.val if l2 else 0
+            val = v1 + v2 + carry
+            carry = val // 10
+            val = val % 10
+            curr.next = ListNode(val)
+            curr = curr.next
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
+
+        return dummy.next
+def printLL(head):
+    curr = head
+    while curr:
+        print(curr.val, end= ' -> ')
+        curr = curr.next
+    print()
+l1 = ListNode(2)
+l1.next = ListNode(4)
+l1.next.next = ListNode(3)
+
+l2 = ListNode(5)
+l2.next = ListNode(6)
+l2.next.next = ListNode(4)
+res = Solution().addTwoNumbers(l1,l2)
+printLL(res)
